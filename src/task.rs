01fe1f44b2f4704e5d40ec830e96226f0d94237c
integerwjo <wanderaotieno321@gmail.com>
2026-04-23 09:20:31 +0300

    initialise task

--- src/task.rs
+++ src/task.rs
@@ -1,10 +1,15 @@
+use std::future::Future;
+use std::pin::Pin;
+type BoxFuture = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
+
 pub struct Task {
-    
+    id: u32,
+    future: BoxFuture
 }
 
 impl Task {
     /// creating a new task
-    fn new() -> Task {
-
+    fn new(id: u32, future: BoxFuture) -> Task {
+        Task { id, future }
     }
 }
