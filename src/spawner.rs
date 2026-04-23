121304a2cf136d5971f28c252c28c7e7dcc510a5
integerwjo <wanderaotieno321@gmail.com>
2026-04-23 20:24:25 +0300

    add docs

---
+++ src/spawner.rs
@@ -0,0 +1,18 @@
+// adding new futures to the executor
+use std::sync::mpsc::{Receiver,  SyncSender};
+use crate::task::Task;
+use std::sync::Arc;
+use std::future::Future;
+
+/// spawner sends tasks to the executor
+pub struct Spawner {
+    pub task_sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    /// creating a new instance of spawner
+    pub fn new(&self, future: impl Future<Output = ()> + Send + 'static) {
+        let task = Arc::new(Task::new(Box::pin(future)));
+        self.task_sender.send(task).expect("Failed to send task");
+    }
+}
